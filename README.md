2f3d6aa1bda7275e714e8f57712900cc782debd7
Nijia Lin <louis70109@gmail.com>
2023-02-04 13:50:00 +0800

    ✨ init

---
+++ README.md
@@ -0,0 +1,16 @@
+# 幫你的 QR Code 加上背景
+
+- QR Code 相關參數請參考 [Awesome-qr.js](https://github.com/SumiMakito/Awesome-qr.js/blob/master/README.md)
+- 建議以深色圖片為主，淺色實測不易掃描。
+
+## 如何開發測試
+
+```
+cd qrcode-background-generator/
+npm install
+npm run dev
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,40 @@
+
+var http = require('http');
+var formidable = require('formidable');
+const { AwesomeQR } = require("awesome-qr");
+const fs = require("fs");
+
+http.createServer(function (req, res) {
+  if (req.url == '/upload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, async function (err, fields, files) {
+      const background = fs.readFileSync(files.upload.filepath);
+      const buffer = await new AwesomeQR({
+        text: fields.url,
+        width: 800,
+        height: 800,
+        typeNumber: 3,
+        colorDark: "#000000",
+        colorLight: "#ffffff",
+        backgroundImage: background,
+        autoColor: true,
+        dotScale:0.35
+      }).draw();
+      
+      // Local testing.
+      fs.writeFileSync("qrcode.png", buffer);
+
+      res.write(buffer);
+      res.end();
+    });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<h3>Default is 800*800</h3><br>');
+    res.write('<form action="upload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="upload"><br><br>');
+    res.write('<input type="text" name="url" placeholder="http://aaa.com" required/><br><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
+}).listen(8080);
